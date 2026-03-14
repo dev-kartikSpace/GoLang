32d167a01e2d2d248997d0d9a7d49a7d3965a3b1
Kartik <kartik200327@gmail.com>
2026-03-14 23:39:44 +0530

    primitive data types

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var myString string
+	myString = "Hello Kartik"
+	//string zero value is emty string
+	fmt.Println(myString)
+
+	var myInt int = 10
+	//int zero value for int is 0
+	fmt.Println(myInt)
+
+	var myBool bool
+	myBool = true
+	//bool zero value for bool is false
+	fmt.Println(myBool)
 }
